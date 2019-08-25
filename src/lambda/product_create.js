3f20765c971c57de4b4ef9f3beb248d4184a102a
Trevor Lane <tlane@simplenexus.com>
2019-08-24 22:53:26 -0600

    test

--- src/lambda/product_create.js
+++ src/lambda/product_create.js
@@ -7,28 +7,35 @@ export async function handler(event, context) {
 
   try {
 
+    // var obj = JSON.parse(event.body)
+
+    getResults();
+
     console.log('test', JSON.parse(event.body))
 
-    var obj = JSON.parse(event.body)
+  //   var obj = JSON.parse(event.body)
 
-    await MongoClient.connect(uri, function(err, client) {
-      if(err) {
-        console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
-      }
-      console.log('Connected...');
-      const collection = client.db("db_tasks").collection("collection_tasks");
-      // perform actions on the collection object
-      collection.insertOne({
-        name: obj.msg,
-        created_at: '08/22/19',
-        assigned_to: 'trevoranthonylane@gmail.com',
-      })
-      client.close();
+  //   await MongoClient.connect(uri, function(err, client) {
+  //     if(err) {
+  //       console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+  //     }
+  //     console.log('Connected...');
+  //     const collection = client.db("db_tasks").collection("collection_tasks");
+  //     // perform actions on the collection object
 
-      console.log('test..')
-  });
 
-  console.log('test...')
+  //     collection.insertOne({
+  //       name: obj.msg,
+  //       created_at: '08/22/19',
+  //       assigned_to: 'trevoranthonylane@gmail.com',
+  //     })
+
+  //     console.log('test..')
+  // })
+
+
+
+  // console.log('test...')
 
 
     return {
@@ -43,3 +50,30 @@ export async function handler(event, context) {
     }
   }
 }
+
+const getResults = async () => {
+  const {client, collection} = await getClient()
+  const result = collection.find({}).toArray()
+  client.close()
+  return result  
+}
+
+const getClient = () => new Promise((resolve, reject) => {
+  const client = new MongoClient(uri, {useNewUrlParser:true})
+
+  client.connect().then((client) => {
+    const db = client.db('db_tasks')
+    const collection = db.collection('collection_tasks')
+
+      collection.insertOne({
+        name: 'test',
+        created_at: '08/22/19',
+        assigned_to: 'trevoranthonylane@gmail.com',
+      })
+
+
+    resolve({client, db, collection})
+  }).catch(reason => reject(reason))
+})
+
+
